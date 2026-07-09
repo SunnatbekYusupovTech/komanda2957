d1e15e63a6ad11584567ea1dc7371e9cba4d7f4c
SUNNATBEE <yusupovsunnatbek32@gmail.com>
2026-07-09 09:54:41 +0500

    Men sunnatbek component tugadi

---
+++ src/components/Sunnatbek.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sunnatbek = () => {
+  return (
+    <div className='bg-red-600 text-white text-9xl text-center'>Sunnatbek</div>
+  )
+}
+
+export default Sunnatbek
